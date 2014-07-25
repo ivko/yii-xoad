fd9b6cac0c71ffdf0d6ae1292ee784907a82a2b9
Ivaylo Velikov <evoolster@gmail.com>
2014-07-25 10:53:16 +0300

    XOAD form - Bootstrap 2

---
+++ views/dialog_bs2.php
@@ -0,0 +1,32 @@
+<!-- Modal -->
+<style>
+    .progress-bar.animate {
+        width: 100%;
+    }
+    .modal .modal-dialog-form .modal-body {
+        padding: 4px 0 4px 0;
+    }
+    .modal .modal-dialog-form .modal-body .form-actions {
+        padding: 19px 20px 20px;
+        margin-top: 0px;
+        text-align: right;
+        background-color: transparent;
+    }
+    .modal .form-wrapper form {
+        padding: 20px 0 0 0;
+        margin: 0;
+    }
+    .modal-body .alert {
+        display: none;
+    }
+</style>
+<div class="modal hide fade" id="modalDialog">
+    <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+        <h3 class="modal-title"></h3>
+    </div>
+    <div class="form-wrapper">
+        
+    </div>
+</div>
+
